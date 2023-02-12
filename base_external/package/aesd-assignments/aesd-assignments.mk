4b4dc836e380d914a6457cb3ae78a1715b397f54
Sujoy Ray <sujoy.ray@colorado.edu>
2023-02-12 09:48:33 -0800

    Adding first check-in for buildroot
    This checkin will push the content to github for
    further testing.
    
    Tested
    ======
    Tested locally using supplied script
    
    Signed-off-by: Sujoy Ray <sujoy.ray@colorado.edu>

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,12 +6,13 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+AESD_ASSIGNMENTS_VERSION = 189ab7579501a9778f9892f94c8e8dae0609c81e
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-sujoyray-ucolorado.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
+
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
@@ -22,7 +23,10 @@ endef
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+    $(INSTALL) -m 0755 $(@D)/finder-app/*.sh $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin/
+    ln -sf /etc/finder-app/conf/  $(TARGET_DIR)/usr/bin/conf
 endef
 
 $(eval $(generic-package))
