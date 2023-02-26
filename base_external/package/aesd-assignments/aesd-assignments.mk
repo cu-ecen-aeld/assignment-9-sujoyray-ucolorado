ae1f8b7c8b64861e901a71f2507f5f67f816fca8
Sujoy Ray <sujoy.ray@colorado.edu>
2023-02-26 14:34:34 -0800

    Adding latest aesdsocket code
    Tested
    ======
    Tested using local script
    
    Signed-off-by: Sujoy Ray <sujoy.ray@colorado.edu>

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 59a623dab057c6d2ba6c8fadc0e468239aec60fa
+AESD_ASSIGNMENTS_VERSION = 474cb139051a7259d642708da54f3eabcf5f11e7
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
