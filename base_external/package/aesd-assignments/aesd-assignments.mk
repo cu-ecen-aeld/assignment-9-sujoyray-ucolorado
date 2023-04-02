f8ced3b969d3af0e23f430c98b973c0f5d5f0d26
Sujoy Ray <sujoy.ray@colorado.edu>
2023-04-02 14:16:15 -0700

    Test using cleaned-up driver
    Tested
    ======
    Tested the code locally using full_test.sh
    
    Signed-off-by: Sujoy Ray <sujoy.ray@colorado.edu>

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = ac11da063aef41f6a216d741a7c575f11ef736fe
+AESD_ASSIGNMENTS_VERSION = 0bee5caa473e9546c7ddc353bf0c17e92d63ae80
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesdchar-assignments/aesdchar.mk
+++ base_external/package/aesdchar-assignments/aesdchar.mk
@@ -7,7 +7,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 7 git contents
-AESDCHAR_ASSIGNMENTS_VERSION = ac11da063aef41f6a216d741a7c575f11ef736fe
+AESDCHAR_ASSIGNMENTS_VERSION = 0bee5caa473e9546c7ddc353bf0c17e92d63ae80
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
